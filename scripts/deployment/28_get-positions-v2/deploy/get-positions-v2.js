38d518b128ff9e7425e97d8ef13a3e48b4a6870d
Vitalii Koval <vito.kovalione@gmail.com>
2022-09-30 13:32:36 +0400

    add GetPositionsV2 deployment scripts

---
+++ scripts/deployment/28_get-positions-v2/deploy/get-positions-v2.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const GetPositionsV2 = artifacts.require('./8.17/stats/GetPositionsV2.sol');
+
+const rawdata = fs.readFileSync('../../../../addresses.json');
+let stablecoinAddress = JSON.parse(rawdata);
+module.exports =  async function(deployer) {
+
+  console.log(">> Deploying an upgradable GetPositionsV2 contract")
+  let promises = [
+    deployer.deploy(GetPositionsV2, { gas: 4050000 }),
+  ];
+
+  await Promise.all(promises);
+
+  const deployed = artifacts.require('./8.17/stats/GetPositionsV2.sol');
+
+  let addressesUpdate = { 
+    getPositionsV2:deployed.address,
+  };
+
+  const newAddresses = {
+    ...stablecoinAddress,  
+    ...addressesUpdate
+  };
+
+  let data = JSON.stringify(newAddresses);
+  fs.writeFileSync('./addresses.json', data);
+};
