a52577b224aa79447fb8d2ecce2a7f5fd7c801ab
DavitMkhitaryan <davitmkhitaryanit@gmail.com>
2022-11-21 13:50:29 +0400

    Added MockPriceOracle Contract setPrice with default price from DEX test

--- scripts/tests/units/delayPriceFeed/DelayFathomOracleDex.test.js
+++ scripts/tests/units/delayPriceFeed/DelayFathomOracleDex.test.js
@@ -7,6 +7,7 @@ const { WeiPerRad, WeiPerRay, WeiPerWad } = require("../../helper/unit");
 const { DeployerAddress, AliceAddress, BobAddress } = require("../../helper/address");
 const { getContract, createMock } = require("../../helper/contracts");
 const { increase } = require('../../helper/time');
+const { weiToRay } = require('../../helper/unit');
 const { formatBytes32String } = ethers.utils
 
 describe("Delay Fathom Oracle with DexPriceOracle - Unit Test Suite", () => {
@@ -17,8 +18,8 @@ describe("Delay Fathom Oracle with DexPriceOracle - Unit Test Suite", () => {
     let mockedBookKeeper;
     let mockedCollateralPoolConfig;
 
-    let dexToken0 = "0xce75A95160D96F5388437993aB5825F322426E04";
-    let dexToken1 = "0x0D2B0406bc8400E61f7507bDed415c98E54A8b11";
+    let dexToken0 = "0x0D2B0406bc8400E61f7507bDed415c98E54A8b11";
+    let dexToken1 = "0xce75A95160D96F5388437993aB5825F322426E04";
     let dexFactoryAddress = "0xb9AdA6B44E4CFF8FE00443Fadf8ad006CfCc2d10";
 
     dexPriceOracle = getContract("DexPriceOracle", DeployerAddress);
@@ -54,7 +55,7 @@ describe("Delay Fathom Oracle with DexPriceOracle - Unit Test Suite", () => {
     describe("DexPriceOracle Contract Tests", () => {
         it("Check getPrice method returns correct default price value from DEX", async () => {
             const returnValue = await dexPriceOracle.getPrice(dexToken0, dexToken1);
-            expect(returnValue[0]).to.be.equal("333333333333333333");
+            expect(returnValue[0]).to.be.equal("3000000000000000000");
         });
     });
 
@@ -68,7 +69,24 @@ describe("Delay Fathom Oracle with DexPriceOracle - Unit Test Suite", () => {
             const returnValue = await delayFathomOraclePriceFeed.callStatic.peekPrice();
             expect(returnValue[0]).to.be.equal(dexPriceOraclePrice[0]);
             expect(returnValue[1]).to.be.true;
-        })
-    })
+        });
+    });
+
+    describe("MockPriceOracle Contract Tests", () => {
+        it("Check setPrice method returns default price from delayPriceFeed <- DexPriceOracle", async () => {
+            const dexPriceOraclePrice = await dexPriceOracle.getPrice(dexToken0, dexToken1);
+            await delayFathomOraclePriceFeed.setTimeDelay(900);
+
+            await delayFathomOraclePriceFeed.peekPrice();
+
+            await delayFathomOraclePriceFeed.callStatic.peekPrice();
+
+            const _collateralPoolId = formatBytes32String("WXDC");
+
+            await mockPriceOracle.setPrice(_collateralPoolId);
+            const _priceWithSafetyMargin = await mockPriceOracle.callStatic.setPrice(_collateralPoolId);
+            await expect(_priceWithSafetyMargin).to.be.equal(weiToRay(dexPriceOraclePrice[0]));
+        });
+    });
 });
 
