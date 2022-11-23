61258d77b777eadff78bffed234185eb0a7b0c89
DavitMkhitaryan <davitmkhitaryanit@gmail.com>
2022-11-23 15:57:25 +0400

    Added tests for bad delay time for setTimeDelay method and tests for setPriceLife method role and bad price life

--- scripts/tests/units/delayPriceFeed/DelayFathomOracleDex.test.js
+++ scripts/tests/units/delayPriceFeed/DelayFathomOracleDex.test.js
@@ -39,29 +39,11 @@ describe("Delay Fathom Oracle with DexPriceOracle - Unit Test Suite", () => {
         await dexPriceOracle.initialize(dexFactoryAddress);
         await delayFathomOraclePriceFeed.initialize(dexPriceOracle.address, dexToken0, dexToken1, accessControlConfig.address);
         await mockPriceOracle.initialize(bookKeeper.address, delayFathomOraclePriceFeed.address, WeiPerRay);
-        // mockedBookKeeper = await createMock("BookKeeper");
-        // mockedCollateralPoolConfig = await createMock("CollateralPoolConfig");
-        // mockedAccessControlConfig = await createMock("AccessControlConfig");
-
-        // await mockedAccessControlConfig.mock.hasRole.returns(true);
-        // await mockedAccessControlConfig.mock.OWNER_ROLE.returns(formatBytes32String("OWNER_ROLE"));
-        // await mockedAccessControlConfig.mock.GOV_ROLE.returns(formatBytes32String("GOV_ROLE"));
-
-        // await mockedBookKeeper.mock.collateralPoolConfig.returns(mockedCollateralPoolConfig.address);
-
-        // await mockedCollateralPoolConfig.mock.collateralPools.priceFeed.returns(delayFathomOraclePriceFeed.address);
-
-        // await mockedCollateralPoolConfig.mock.getLiquidationRatio.returns(WeiPerRay);
-
-        //if accounts[0] is checked whether having role or not,
-        //set up accessPoolConfig.hasRole()
-        // accessControlConfig.hasRole(accessControlConfig.OWNER_ROLE(), msg.sender
-
-        // console.log("mockedAccessControlConfig address is " + mockedAccessControlConfig.address);
-        // console.log("mockedBookKeeper address is " + mockedBookKeeper.address);
     });
 
     describe("DexPriceOracle Contract Tests", () => {
+
+        // getPrice method tests
         it("Check getPrice method returns correct default price from DEX", async () => {
             const returnValue = await dexPriceOracle.getPrice(dexToken0, dexToken1);
             expect(returnValue[0]).to.be.equal("3000000000000000000");
@@ -74,13 +56,15 @@ describe("Delay Fathom Oracle with DexPriceOracle - Unit Test Suite", () => {
     });
 
     describe("DelayFathomOraclePriceFeed Contract Tests", () => {
+
+        // setTimeDelay method tests
         it("Check setTimeDelay method reverts with !ownerRole when calling it with address having no role", async () => {
             const delayFathomOraclePriceFeed2 = await connectToContractWithAddress(delayFathomOraclePriceFeed, AliceAddress);
             await expect(delayFathomOraclePriceFeed2.setTimeDelay(900)).to.be.revertedWith("!ownerRole");
         });
 
         it("Check setTimeDelay method reverts with !ownerRole when calling it with address having GOV role", async () => {
-            await accessControlConfig.grantRole(accessControlConfig.GOV_ROLE(),BobAddress, { gasLimit: 1000000 });
+            await accessControlConfig.grantRole(accessControlConfig.GOV_ROLE(), BobAddress, { gasLimit: 1000000 });
 
             const delayFathomOraclePriceFeed2 = await connectToContractWithAddress(delayFathomOraclePriceFeed, BobAddress);
             await expect(delayFathomOraclePriceFeed2.setTimeDelay(900)).to.be.revertedWith("!ownerRole");
@@ -92,6 +76,47 @@ describe("Delay Fathom Oracle with DexPriceOracle - Unit Test Suite", () => {
             expect(returnValue).to.be.eq(900);
         });
 
+        it("Check setTimeDelay method reverts with 'FathomOraclePriceFeed/bad-delay-time' when parameter is less than 900(seconds) / 15 minutes", async () => {
+            await expect(delayFathomOraclePriceFeed.setTimeDelay(899)).to.be.revertedWith("FathomOraclePriceFeed/bad-delay-time");
+        });
+
+        it("Check setTimeDelay method reverts with 'FathomOraclePriceFeed/bad-delay-time' when parameter is more than 86,400(seconds) / 1 day", async () => {
+            await expect(delayFathomOraclePriceFeed.setTimeDelay(86401)).to.be.revertedWith("FathomOraclePriceFeed/bad-delay-time");
+        });
+
+
+        //setPriceLife method tests
+        it("Check setPriceLife method reverts with !ownerRole when calling it with address having no role", async () => {
+            const delayFathomOraclePriceFeed2 = await connectToContractWithAddress(delayFathomOraclePriceFeed, AliceAddress);
+            await expect(delayFathomOraclePriceFeed2.setPriceLife(3600)).to.be.revertedWith("!ownerRole");
+        });
+
+        it("Check setPriceLife method reverts with !ownerRole when calling it with address having GOV role", async () => {
+            await accessControlConfig.grantRole(accessControlConfig.GOV_ROLE(), BobAddress, { gasLimit: 1000000 });
+
+            const delayFathomOraclePriceFeed2 = await connectToContractWithAddress(delayFathomOraclePriceFeed, BobAddress);
+            await expect(delayFathomOraclePriceFeed2.setTimeDelay(3600)).to.be.revertedWith("!ownerRole");
+        });
+
+        it("Check setPriceLife method succeeds when calling it with address having OWNER role", async () => {
+            await delayFathomOraclePriceFeed.setPriceLife(3600);
+            const returnValue = await delayFathomOraclePriceFeed.priceLife();
+            expect(returnValue).to.be.eq(3600);
+        });
+
+        it("Check setPriceLife method reverts with 'FathomOraclePriceFeed/bad-price-life' when parameter is less than 3600(seconds) / 1 hour", async () => {
+            await expect(delayFathomOraclePriceFeed.setPriceLife(3599)).to.be.revertedWith("FathomOraclePriceFeed/bad-price-life");
+        });
+
+        it("Check setPriceLife method reverts with 'FathomOraclePriceFeed/bad-price-life' when parameter is more than 86,400(seconds) / 1 day", async () => {
+            await expect(delayFathomOraclePriceFeed.setPriceLife(86401)).to.be.revertedWith("FathomOraclePriceFeed/bad-price-life");
+        });
+
+        // pause method tests
+
+        //unpause method tests
+
+        // peekPrice method tests
         it("Check peekPrice method returns default price from DexPriceOracle when current price is 0 and delay time has not passed", async () => {
             const dexPriceOraclePrice = await dexPriceOracle.getPrice(dexToken0, dexToken1);
 
@@ -103,6 +128,7 @@ describe("Delay Fathom Oracle with DexPriceOracle - Unit Test Suite", () => {
             expect(returnValue[1]).to.be.true;
         });
 
+        // readPrice method tests
         it("Check readPrice method returns default price from DexPriceOracle after calling peekPrice", async () => {
             const dexPriceOraclePrice = await dexPriceOracle.getPrice(dexToken0, dexToken1);
             await delayFathomOraclePriceFeed.setTimeDelay(900);
@@ -114,6 +140,8 @@ describe("Delay Fathom Oracle with DexPriceOracle - Unit Test Suite", () => {
     });
 
     describe("MockPriceOracle Contract Tests", () => {
+
+        // setPrice method tests
         it("Check setPrice method returns default price from DelayPriceFeed when DexPriceOracle price is also the default one", async () => {
             const dexPriceOraclePrice = await dexPriceOracle.getPrice(dexToken0, dexToken1);
             await delayFathomOraclePriceFeed.setTimeDelay(900);
