f4ebc94b7c720a1c4544dd8f33a2f5dcaa8d6d2e
codinghistorian <gaudihaus@gmail.com>
2022-11-24 14:07:18 +0400

    commit before sync up with Davit

---
+++ scripts/tests/units/delayPriceFeed/Swap.test.js
@@ -0,0 +1,33 @@
+const { getDeadlineTimestamp } = require("../../helper/timeStamp");
+const { getContract } = require("../../helper/contracts");
+const { approve } = require("../../helper/token");
+
+
+//1)approve
+//2)Include DEX RouterV2 Interface to the codebase, so that the contract can be instantiated.
+//3)swap.
+
+describe("Swap tokens on dex", async () => {
+  
+    let mockToken0 = "0xce75A95160D96F5388437993aB5825F322426E04"; // <- some address from Remix
+    let mockToken1 = "0x0D2B0406bc8400E61f7507bDed415c98E54A8b11"; // <- some address from Remix
+    let routerAddress = "0xf72f1a39ae0736Ef6A532605C85aFB0A4E349714"; // DEX Router Address
+
+    const Router = await artifacts.initializeInterfaceAt("IUniswapV2Router01", routerAddress);
+
+  
+    beforeEach(async () => {
+      await snapshot.revertToSnapshot();
+
+      //approve
+      await approve(mockToken0, routerAddress, 200000);
+      await approve(mockToken1, routerAddress, 200000);
+    })
+  
+    describe("Swap tokens on DEX", () => {
+      it("Swap", async () => {
+        await Router.swapExactTokensForTokens(100, 200, [mockToken0, mockToken1], "0x4C5F0f90a2D4b518aFba11E22AC9b8F6B031d204", await getDeadlineTimestamp(10000));
+      });
+
+    });
+  });
