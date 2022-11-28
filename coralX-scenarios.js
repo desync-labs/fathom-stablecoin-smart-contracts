5a6124273e1e9249395b7c36116ecf441e8f806b
Vitalii Koval <vito.kovalione@gmail.com>
2022-11-28 11:59:39 +0400

    remove redundant scenario

--- coralX-scenarios.js
+++ coralX-scenarios.js
@@ -15,13 +15,6 @@ module.exports = {
     ['compile'],
     ['execute', '--path', 'scripts/migrations/collateral-tokens', '--network', 'apothem']
   ],
-  deployApothem: [
-    ['execute', '--path', 'scripts/migrations/compliance-upgradability', '--network', 'apothem'],
-    ['execute', '--path', 'scripts/migrations/tokens-creation-service', '--network', 'apothem'],
-    ['execute', '--path', 'scripts/configurations/1_base_config.js', '--network', 'apothem'],
-    ['execute', '--path', 'scripts/configurations/2_tokens_factory.js', '--network', 'apothem'],
-    ['execute', '--path', 'scripts/custom/white-list-setup.js', '--network', 'apothem'],
-  ],
   migrateAndConfigureForTests: [
     ['compile'],
     ['execute', '--path', 'scripts/migrations/tests'],
