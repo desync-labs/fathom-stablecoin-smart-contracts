0ac5802d2a5b9119879345a0ac7a8c9eaf6ffb01
Vitalii Koval <vito.kovalione@gmail.com>
2023-01-05 15:06:41 +0400

    working on tests

--- scripts/tests/helper/positions.js
+++ scripts/tests/helper/positions.js
@@ -29,6 +29,32 @@ const openPositionAndDraw = async (proxyWallet, from, collateral_pool_id, collat
     await proxyWallet.execute2(fathomStablecoinProxyActions.address, openPositionCall, { from: from })
 }
 
+
+const openXDCPositionAndDraw = async (proxyWallet, from, collateral_pool_id, collateral, stablecoin) => {
+    const proxyFactory = await artifacts.initializeInterfaceAt("FathomProxyFactory", "FathomProxyFactory");
+    const positionManager = await getProxy(proxyFactory, "PositionManager");
+    const stablecoinAdapter = await getProxy(proxyFactory, "StablecoinAdapter");
+    const ankrCollateralAdapter = await getProxy(proxyFactory, "AnkrCollateralAdapter");
+    const stabilityFeeCollector = await getProxy(proxyFactory, "StabilityFeeCollector");
+    const fathomStablecoinProxyActions = await artifacts.initializeInterfaceAt("FathomStablecoinProxyActions", "FathomStablecoinProxyActions");
+
+    const abi = [
+        "function openLockXDCAndDraw(address _manager, address _stabilityFeeCollector, address _xdcAdapter, address _stablecoinAdapter, bytes32 _collateralPoolId, uint256 _stablecoinAmount, bytes calldata _data)"
+    ];
+
+    const iFace = new ethers.utils.Interface(abi);
+    const call = iFace.encodeFunctionData("openLockXDCAndDraw", [
+        positionManager.address,
+        stabilityFeeCollector.address,
+        ankrCollateralAdapter.address,
+        stablecoinAdapter.address,
+        collateral_pool_id,
+        stablecoin,
+        ethers.utils.defaultAbiCoder.encode(["address"], [from]),
+    ])
+    await proxyWallet.execute2(fathomStablecoinProxyActions.address, call, {  value: collateral,  from: from, gasLimit: 2000000 })
+}
+
 const openPosition = async (proxyWallet, from, collateral_pool_id) => {
     const proxyFactory = await artifacts.initializeInterfaceAt("FathomProxyFactory", "FathomProxyFactory");
     const positionManager = await getProxy(proxyFactory, "PositionManager");
@@ -66,6 +92,54 @@ const wipeAndUnlockToken = async (proxyWallet, from, tokenAdapter, stablecoinAda
     ])
 
     await proxyWallet.execute2(fathomStablecoinProxyActions.address, wipeAndUnlockTokenCall, { from: from })
+}
+
+const wipeAndUnlockXDC = async (proxyWallet, from, positionId, collateral, stablecoin) => {
+    const proxyFactory = await artifacts.initializeInterfaceAt("FathomProxyFactory", "FathomProxyFactory");
+    const positionManager = await getProxy(proxyFactory, "PositionManager");
+    const fathomStablecoinProxyActions = await artifacts.initializeInterfaceAt("FathomStablecoinProxyActions", "FathomStablecoinProxyActions");
+    const ankrCollateralAdapter = await getProxy(proxyFactory, "AnkrCollateralAdapter");
+    const stablecoinAdapter = await getProxy(proxyFactory, "StablecoinAdapter");
+
+    const abi = [
+        "function wipeAndUnlockXDC(address _manager, address _xdcAdapter, address _stablecoinAdapter, uint256 _positionId, uint256 _collateralAmount, uint256 _stablecoinAmount, bytes calldata _data)"
+    ];
+    const iFace = new ethers.utils.Interface(abi);
+    const call = iFace.encodeFunctionData("wipeAndUnlockXDC", [
+        positionManager.address,
+        ankrCollateralAdapter.address,
+        stablecoinAdapter.address,
+        positionId,
+        collateral,
+        stablecoin,
+        ethers.utils.defaultAbiCoder.encode(["address"], [from]),
+    ])
+
+    await proxyWallet.execute2(fathomStablecoinProxyActions.address, call, {from: from })
+}
+
+
+const wipeAllAndUnlockXDC = async (proxyWallet, from, positionId, collateral) => {
+    const proxyFactory = await artifacts.initializeInterfaceAt("FathomProxyFactory", "FathomProxyFactory");
+    const positionManager = await getProxy(proxyFactory, "PositionManager");
+    const fathomStablecoinProxyActions = await artifacts.initializeInterfaceAt("FathomStablecoinProxyActions", "FathomStablecoinProxyActions");
+    const ankrCollateralAdapter = await getProxy(proxyFactory, "AnkrCollateralAdapter");
+    const stablecoinAdapter = await getProxy(proxyFactory, "StablecoinAdapter");
+
+    const abi = [
+        "function wipeAllAndUnlockXDC(address _manager, address _xdcAdapter, address _stablecoinAdapter, uint256 _positionId, uint256 _collateralAmount, bytes calldata _data)"
+    ];
+    const iFace = new ethers.utils.Interface(abi);
+    const call = iFace.encodeFunctionData("wipeAllAndUnlockXDC", [
+        positionManager.address,
+        ankrCollateralAdapter.address,
+        stablecoinAdapter.address,
+        positionId,
+        collateral,
+        ethers.utils.defaultAbiCoder.encode(["address"], [from]),
+    ])
+
+    await proxyWallet.execute2(fathomStablecoinProxyActions.address, call, {from: from })
 }
 
 const lockToken = async (proxyWallet, from, collateral_pool_id, positionId, amount) => {
@@ -265,8 +339,11 @@ const importPosition = async (proxyWallet, from, source, positionId) => {
 
 module.exports = {
     openPositionAndDraw,
+    openXDCPositionAndDraw,
     openPosition,
     wipeAndUnlockToken,
+    wipeAndUnlockXDC,
+    wipeAllAndUnlockXDC,
     lockToken,
     draw,
     moveCollateral,
--- scripts/tests/integration/StabilityFeeCollector.test.js
+++ scripts/tests/integration/StabilityFeeCollector.test.js
@@ -12,8 +12,7 @@ const { formatBytes32String } = require("ethers/lib/utils");
 const PositionHelper = require("../helper/positions");
 const { loadFixture } = require("../helper/fixtures");
 const { getProxy } = require("../../common/proxies");
-
-const COLLATERAL_POOL_ID = formatBytes32String("WXDC")
+const pools = require("../../common/collateral");
 
 const CLOSE_FACTOR_BPS = BigNumber.from(5000)
 const LIQUIDATOR_INCENTIVE_BPS = BigNumber.from(12500)
@@ -22,7 +21,7 @@ const TREASURY_FEE_BPS = BigNumber.from(2500)
 const setup = async () => {
     const proxyFactory = await artifacts.initializeInterfaceAt("FathomProxyFactory", "FathomProxyFactory");
 
-    const collateralTokenAdapterFactory = await getProxy(proxyFactory, "CollateralTokenAdapterFactory");
+    // const collateralTokenAdapterFactory = await getProxy(proxyFactory, "CollateralTokenAdapterFactory");
     const collateralPoolConfig = await getProxy(proxyFactory, "CollateralPoolConfig");
     const bookKeeper = await getProxy(proxyFactory, "BookKeeper");
     const simplePriceFeed = await getProxy(proxyFactory, "SimplePriceFeed");
@@ -32,41 +31,41 @@ const setup = async () => {
     const fathomStablecoin = await getProxy(proxyFactory, "FathomStablecoin");
     const priceOracle = await getProxy(proxyFactory, "PriceOracle");
 
-    const collateralTokenAdapterAddress = await collateralTokenAdapterFactory.adapters(COLLATERAL_POOL_ID)
-    const tokenAdapter = await artifacts.initializeInterfaceAt("CollateralTokenAdapter", collateralTokenAdapterAddress);
-    const wxdcAddr = await tokenAdapter.collateralToken();
-    const WXDC = await artifacts.initializeInterfaceAt("ERC20Mintable", wxdcAddr);
+    // const collateralTokenAdapterAddress = await collateralTokenAdapterFactory.adapters(pools.XDC)
+    // const tokenAdapter = await artifacts.initializeInterfaceAt("CollateralTokenAdapter", collateralTokenAdapterAddress);
+    // const wxdcAddr = await tokenAdapter.collateralToken();
+    // const WXDC = await artifacts.initializeInterfaceAt("ERC20Mintable", wxdcAddr);
 
     ({
         proxyWallets: [aliceProxyWallet],
     } = await createProxyWallets([AliceAddress]));
 
     await stabilityFeeCollector.setSystemDebtEngine(DevAddress)
-    await collateralPoolConfig.initCollateralPool(
-        COLLATERAL_POOL_ID,
-        WeiPerRad.mul(100),
-        WeiPerRad.mul(1),
-        simplePriceFeed.address,
-        WeiPerRay,
-        WeiPerRay,
-        tokenAdapter.address,
-        CLOSE_FACTOR_BPS,
-        LIQUIDATOR_INCENTIVE_BPS,
-        TREASURY_FEE_BPS,
-        AddressZero
-    )
+    // await collateralPoolConfig.initCollateralPool(
+    //     pools.XDC,
+    //     WeiPerRad.mul(100),
+    //     WeiPerRad.mul(1),
+    //     simplePriceFeed.address,
+    //     WeiPerRay,
+    //     WeiPerRay,
+    //     tokenAdapter.address,
+    //     CLOSE_FACTOR_BPS,
+    //     LIQUIDATOR_INCENTIVE_BPS,
+    //     TREASURY_FEE_BPS,
+    //     AddressZero
+    // )
 
     await simplePriceFeed.setPrice(WeiPerRay, { gasLimit: 1000000 })
-    await bookKeeper.setTotalDebtCeiling(WeiPerRad.mul(100), { gasLimit: 1000000 })
-    await WXDC.approve(aliceProxyWallet.address, WeiPerWad.mul(10000), { from: AliceAddress })
+    // await bookKeeper.setTotalDebtCeiling(WeiPerRad.mul(100), { gasLimit: 1000000 })
+    // await WXDC.approve(aliceProxyWallet.address, WeiPerWad.mul(10000), { from: AliceAddress })
     await fathomStablecoin.approve(aliceProxyWallet.address, WeiPerWad.mul(10000), { from: AliceAddress })
-    await priceOracle.setPrice(COLLATERAL_POOL_ID)
+    await priceOracle.setPrice(pools.XDC, { gasLimit: 2000000 })
 
     return {
         bookKeeper,
         stablecoinAdapter,
         positionManager,
-        tokenAdapter,
+        // tokenAdapter,
         stabilityFeeCollector,
         simplePriceFeed,
         collateralPoolConfig,
@@ -81,7 +80,7 @@ describe("Stability Fee", () => {
     // Contract
     let positionManager
     let bookKeeper
-    let tokenAdapter
+    // let tokenAdapter
     let stablecoinAdapter
     let stabilityFeeCollector
     let collateralPoolConfig
@@ -96,7 +95,7 @@ describe("Stability Fee", () => {
             bookKeeper,
             stablecoinAdapter,
             positionManager,
-            tokenAdapter,
+            // tokenAdapter,
             stabilityFeeCollector,
             simplePriceFeed,
             collateralPoolConfig,
@@ -107,13 +106,13 @@ describe("Stability Fee", () => {
     describe("#collect", () => {
         context("when call collect directly and call deposit", () => {
             it("should be success", async () => {
-                await collateralPoolConfig.setStabilityFeeRate(COLLATERAL_POOL_ID, BigNumber.from("1000000005781378656804591713"), { gasLimit: 1000000 })
+                await collateralPoolConfig.setStabilityFeeRate(pools.XDC, BigNumber.from("1000000005781378656804591713"), { gasLimit: 1000000 })
 
                 // time increase 6 month
                 await TimeHelpers.increase(TimeHelpers.duration.seconds(BigNumber.from("15768000")))
                 await simplePriceFeed.setPrice(WeiPerRay, { gasLimit: 1000000 })
-                await stabilityFeeCollector.collect(COLLATERAL_POOL_ID, { gasLimit: 1000000 })
-                const debtAccumulatedRate = await collateralPoolConfig.collateralPools(COLLATERAL_POOL_ID)
+                await stabilityFeeCollector.collect(pools.XDC, { gasLimit: 1000000 })
+                const debtAccumulatedRate = await collateralPoolConfig.collateralPools(pools.XDC)
                 // debtAccumulatedRate = RAY(1000000005781378656804591713^15768000) = 1095445115010332226911367294
                 AssertHelpers.assertAlmostEqual(
                     (debtAccumulatedRate).debtAccumulatedRate.toString(),
@@ -125,21 +124,21 @@ describe("Stability Fee", () => {
                 //  a. open a new position
                 //  b. lock WXDC
                 //  c. mint FUSD
-                await PositionHelper.openPositionAndDraw(aliceProxyWallet, AliceAddress, COLLATERAL_POOL_ID, WeiPerWad.mul(10), WeiPerWad.mul(5))
+                await PositionHelper.openXDCPositionAndDraw(aliceProxyWallet, AliceAddress, pools.XDC, WeiPerWad.mul(10), WeiPerWad.mul(5))
                 const positionId = await positionManager.ownerLastPositionId(aliceProxyWallet.address)
                 const positionAddress = await positionManager.positions(positionId)
 
                 // position debtShare = 5000000000000000000000000000000000000000000000 / 1095445115010332226911367294 = 4564354645876384278
                 AssertHelpers.assertAlmostEqual(
-                    (await bookKeeper.positions(COLLATERAL_POOL_ID, positionAddress)).debtShare.toString(),
+                    (await bookKeeper.positions(pools.XDC, positionAddress)).debtShare.toString(),
                     "4564354645876384278"
                 )
                 AssertHelpers.assertAlmostEqual(
-                    (await collateralPoolConfig.collateralPools(COLLATERAL_POOL_ID)).totalDebtShare.toString(),
+                    (await collateralPoolConfig.collateralPools(pools.XDC)).totalDebtShare.toString(),
                     "4564354645876384278"
                 )
                 AssertHelpers.assertAlmostEqual(
-                    (await collateralPoolConfig.collateralPools(COLLATERAL_POOL_ID)).debtAccumulatedRate.toString(),
+                    (await collateralPoolConfig.collateralPools(pools.XDC)).debtAccumulatedRate.toString(),
                     "1095445115010332226911367294"
                 )
 
@@ -151,14 +150,14 @@ describe("Stability Fee", () => {
                 //  a. open a new position
                 //  b. lock WXDC
                 //  c. mint FUSD
-                await PositionHelper.openPositionAndDraw(aliceProxyWallet, AliceAddress, COLLATERAL_POOL_ID, WeiPerWad.mul(10), WeiPerWad.mul(5))
+                await PositionHelper.openXDCPositionAndDraw(aliceProxyWallet, AliceAddress, pools.XDC, WeiPerWad.mul(10), WeiPerWad.mul(5))
 
                 const positionId2 = await positionManager.ownerLastPositionId(aliceProxyWallet.address)
                 const positionAddress2 = await positionManager.positions(positionId2)
 
                 // debtAccumulatedRate = RAY((1000000005781378656804591713^31536000) * 1095445115010332226911367294) = 1314534138012398672287467301
                 AssertHelpers.assertAlmostEqual(
-                    (await collateralPoolConfig.collateralPools(COLLATERAL_POOL_ID)).debtAccumulatedRate.toString(),
+                    (await collateralPoolConfig.collateralPools(pools.XDC)).debtAccumulatedRate.toString(),
                     "1314534138012398672287467301"
                 )
                 // debtShare * diffDebtAccumulatedRate =  4564354645876384278 * (1314534138012398672287467301 - 1095445115010332226911367294) = 999999999999999999792432233173942358090489946
@@ -169,12 +168,12 @@ describe("Stability Fee", () => {
 
                 // position debtShare = 5000000000000000000000000000000000000000000000 / 1314534138012398672287467301 = 3803628871563653565
                 AssertHelpers.assertAlmostEqual(
-                    (await bookKeeper.positions(COLLATERAL_POOL_ID, positionAddress2)).debtShare.toString(),
+                    (await bookKeeper.positions(pools.XDC, positionAddress2)).debtShare.toString(),
                     "3803628871563653565"
                 )
                 // 4564354645876384278 + 3803628871563653565 = 8367983517440037843
                 AssertHelpers.assertAlmostEqual(
-                    (await collateralPoolConfig.collateralPools(COLLATERAL_POOL_ID)).totalDebtShare.toString(),
+                    (await collateralPoolConfig.collateralPools(pools.XDC)).totalDebtShare.toString(),
                     "8367983517440037843"
                 )
 
@@ -183,11 +182,11 @@ describe("Stability Fee", () => {
                 await simplePriceFeed.setPrice(WeiPerRay, { gasLimit: 1000000 })
 
                 // debtAccumulatedRate ~ 20%
-                await stabilityFeeCollector.collect(COLLATERAL_POOL_ID, { gasLimit: 1000000 })
+                await stabilityFeeCollector.collect(pools.XDC, { gasLimit: 1000000 })
 
                 // debtAccumulatedRate = RAY((1000000005781378656804591713^31536000) * 1314534138012398672287467301) = 1577440965614878406737552619
                 AssertHelpers.assertAlmostEqual(
-                    (await collateralPoolConfig.collateralPools(COLLATERAL_POOL_ID)).debtAccumulatedRate.toString(),
+                    (await collateralPoolConfig.collateralPools(pools.XDC)).debtAccumulatedRate.toString(),
                     "1577440965614878406737552619"
                 )
                 // debtShare * diffDebtAccumulatedRate =  8367983517440037843 * (1577440965614878406737552619 - 1314534138012398672287467301) = 2199999999999999999533019044066331740498689074
@@ -199,18 +198,16 @@ describe("Stability Fee", () => {
 
                 //  a. repay some FUSD
                 //  b. alice unlock some WXDC
-                await PositionHelper.wipeAndUnlockToken(
+                await PositionHelper.wipeAndUnlockXDC(
                     aliceProxyWallet,
                     AliceAddress,
-                    tokenAdapter.address,
-                    stablecoinAdapter.address,
                     positionId,
                     WeiPerWad,
                     WeiPerWad
                 )
 
                 AssertHelpers.assertAlmostEqual(
-                    (await collateralPoolConfig.collateralPools(COLLATERAL_POOL_ID)).debtAccumulatedRate.toString(),
+                    (await collateralPoolConfig.collateralPools(pools.XDC)).debtAccumulatedRate.toString(),
                     "1577440965614878406737552619"
                 )
                 AssertHelpers.assertAlmostEqual(
