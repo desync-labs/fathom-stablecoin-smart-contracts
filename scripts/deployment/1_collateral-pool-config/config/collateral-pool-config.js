3ca3f98ed55a39bfda30e894b361eeefb7766029
codinghistorian <gaudihaus@gmail.com>
2023-01-11 11:18:30 +0400

    commit before Tarun-ji forks from it

--- scripts/deployment/1_collateral-pool-config/config/collateral-pool-config.js
+++ scripts/deployment/1_collateral-pool-config/config/collateral-pool-config.js
@@ -8,9 +8,9 @@ const WeiPerWad = BigNumber.from(`1${"0".repeat(18)}`)
 const WeiPerRay = BigNumber.from(`1${"0".repeat(27)}`)
 const WeiPerRad = BigNumber.from(`1${"0".repeat(45)}`)
 const COLLATERAL_POOL_ID = formatBytes32String("XDC")
-const CLOSE_FACTOR_BPS = BigNumber.from(5000)   // <- 0.5
+const CLOSE_FACTOR_BPS = BigNumber.from(2500)   // <- 0.5
 const LIQUIDATOR_INCENTIVE_BPS = BigNumber.from(10500)  // <- 1.05
-const TREASURY_FEE_BPS = BigNumber.from(5000) // <- 0.5 50%
+const TREASURY_FEE_BPS = BigNumber.from(8000) // <- 0.5 50%
 
 const rawdata = fs.readFileSync('../../../../addresses.json');
 let stablecoinAddress = JSON.parse(rawdata);
