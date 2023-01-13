870e8f15ed8d5795ad02bddb46d509c8d8a00c92
codinghistorian <gaudihaus@gmail.com>
2023-01-13 16:14:32 +0400

    minter role in test/config back

--- scripts/migrations/test/configuration/1_add-roles.js
+++ scripts/migrations/test/configuration/1_add-roles.js
@@ -9,6 +9,10 @@ module.exports =  async function(deployer) {
     const fathomStablecoin = await getProxy(proxyFactory, "FathomStablecoin");
     const accessControlConfig = await getProxy(proxyFactory, "AccessControlConfig");
   
+    await fathomStablecoin.grantRole(await fathomStablecoin.MINTER_ROLE(), DeployerWallet);
+
     await accessControlConfig.grantRole(await accessControlConfig.PRICE_ORACLE_ROLE(), DeployerWallet)
     await accessControlConfig.grantRole(await accessControlConfig.OWNER_ROLE(), DeployerWallet)
+
+    await accessControlConfig.grantRole(await accessControlConfig.MINTABLE_ROLE(), DeployerWallet)
 }
