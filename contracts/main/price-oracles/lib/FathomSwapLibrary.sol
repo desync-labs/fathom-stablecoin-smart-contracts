8bece245321189d5df7928d2e9cd55a9d0426fa1
codinghistorian <gaudihaus@gmail.com>
2023-02-03 20:52:12 +0400

    commit in progress of library fix

--- contracts/main/price-oracles/lib/FathomSwapLibrary.sol
+++ contracts/main/price-oracles/lib/FathomSwapLibrary.sol
@@ -17,14 +17,40 @@ library FathomSwapLibrary {
 
     function getReserves(address factory, address tokenA, address tokenB) internal view returns (uint reserveA, uint reserveB) {
         (address token0, ) = sortTokens(tokenA, tokenB);
-        address pair = pairFor(factory, tokenA, tokenB);
+ 
+        (bool success, bytes memory data1) = factory.staticcall(abi.encodeWithSignature("getPair(address,address)", tokenA, tokenB));
+
+
+        bytes memory empty;
+        //a) check if factory was wrong
+        //if data1 returns empty bytes array, that means factory address had no factory getPair getter fn
+        require(keccak256(data1) != keccak256(empty), "wrong factory address");
+        //b) check if pair address is address(0) or not.
+        //even if factory address was correct, the pair may not exist
+        //revert if it is address(0)
+        require(keccak256(data1) != keccak256(abi.encode(0)), "pair nonexistent");
+
+        //c) pair needs to be converted to address
+        //bytes memory to address
+        //bytes memory data1 -> address...hm...
+        address pair;
+        assembly {
+            pair := data1
+        }
+
         (bool success, bytes memory data) = pair.staticcall(abi.encodeWithSignature("getReserves()"));
+        require(data.length == 0x60, "invalid data length")
         require(success, "getReserves call failed");
+
+        //d) data length check
         uint256 reserve0;
         uint256 reserve1;
         assembly {
             let fmp := mload(0x40)
             mstore(fmp, data)
+            if iszero(data == 0x0) {
+                revert
+            }
             reserve1 := mload(sub(fmp, 0x40))
             reserve0 := mload(sub(fmp, 0x60))
         }
