a9aae3003cee3507ab30b06e869c0d0fd9e61d28
ssubik <shresthasubik@gmail.com>
2023-04-07 17:09:43 +0545

    added IERC20 to IStablecoin instead of IToken

--- contracts/main/interfaces/IStablecoin.sol
+++ contracts/main/interfaces/IStablecoin.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.8.17;
 
-import "./IToken.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-interface IStablecoin is IToken {
+interface IStablecoin is IERC20 {
     function mint(address, uint256) external;
 
     function burn(address, uint256) external;
