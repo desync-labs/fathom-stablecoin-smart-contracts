86eeb1506457c679554f20383d73591f7e66f28b
codinghistorian <gaudihaus@gmail.com>
2023-04-27 14:00:55 +0400

    delete comment on mocksimplepricefeed

--- contracts/tests/mocks/MockSimplePriceFeed.sol
+++ contracts/tests/mocks/MockSimplePriceFeed.sol
@@ -41,8 +41,6 @@ contract MockSimplePriceFeed is PausableUpgradeable, AccessControlUpgradeable, I
 
     event LogSetPrice(address indexed _caller, uint256 _price, uint256 indexed _lastUpdate);
 
-    // event LogSetPriceLife(address indexed _caller, uint256 _second);
-
     /// @dev access: OWNER_ROLE
     function setPrice(uint256 _price) external onlyOwner {
         price = _price;
