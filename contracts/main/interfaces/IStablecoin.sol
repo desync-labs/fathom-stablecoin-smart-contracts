eb0a02be2fee533b85db756df278de653a3abc03
Anton Grigorev <anton.grigorev.dev@gmail.com>
2023-05-02 13:45:41 +0400

    Update IStablecoin.sol

--- contracts/main/interfaces/IStablecoin.sol
+++ contracts/main/interfaces/IStablecoin.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.17;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IStablecoin is IERC20 {
+    event Rename(string name);
+
     function mint(address, uint256) external;
 
     function burn(address, uint256) external;
