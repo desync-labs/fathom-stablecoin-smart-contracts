14e3106242ac433a5d76226f5f91b9b584b4894d
Vitalii Koval <vitalii.koval@securrency.com>
2023-07-10 13:24:50 +0400

    fix comments

--- contracts/main/stablecoin-core/liquidation-strategies/FixedSpreadLiquidationStrategy.sol
+++ contracts/main/stablecoin-core/liquidation-strategies/FixedSpreadLiquidationStrategy.sol
@@ -136,11 +136,11 @@ contract FixedSpreadLiquidationStrategy is FixedSpreadLiquidationStrategyMath, P
     // solhint-disable function-max-lines
     function execute(
         bytes32 _collateralPoolId,
-        uint256 _positionDebtShare, // Debt Value                  [rad]
+        uint256 _positionDebtShare, // Debt Value                  [wad]
         uint256 _positionCollateralAmount, // Collateral Amount           [wad]
         address _positionAddress, // Address that will receive any leftover collateral
-        uint256 _debtShareToBeLiquidated, // The value of debt to be liquidated as specified by the liquidator [rad]
-        uint256 _maxDebtShareToBeLiquidated, // The maximum value of debt to be liquidated as specified by the liquidator in case of full liquidation for slippage control [rad]
+        uint256 _debtShareToBeLiquidated, // The value of debt to be liquidated as specified by the liquidator [wad]
+        uint256 _maxDebtShareToBeLiquidated, // The maximum value of debt to be liquidated as specified by the liquidator in case of full liquidation for slippage control [wad]
         address _liquidatorAddress,
         address _collateralRecipient,
         bytes calldata _data // Data to pass in external call; if length 0, no call is done
