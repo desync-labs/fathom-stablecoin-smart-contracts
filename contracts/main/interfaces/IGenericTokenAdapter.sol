d9c1a4d0cda8fdcbc0ba7c5fe01bbc7af8c5f4a9
codinghistorian <gaudihaus@gmail.com>
2023-07-14 14:47:48 +0400

    Revert "tokenAdapter adjustment"
    This reverts commit 1eb4b7bc2789c8c1da95b6b240743a423527fa7d.

--- contracts/main/interfaces/IGenericTokenAdapter.sol
+++ contracts/main/interfaces/IGenericTokenAdapter.sol
@@ -10,6 +10,10 @@ interface IGenericTokenAdapter {
 
     function withdraw(address positionAddress, uint256 wad, bytes calldata data) external;
 
+    function onAdjustPosition(address src, address dst, int256 collateralValue, int256 debtShare, bytes calldata data) external;
+
+    function onMoveCollateral(address src, address dst, uint256 wad, bytes calldata data) external;
+
     function collateralToken() external returns (address);
 
     function collateralPoolId() external view returns (bytes32);
