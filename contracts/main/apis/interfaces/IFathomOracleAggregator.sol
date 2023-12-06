e26170b30a1f1c0b72b36cca7d3cbcb5016a5ec7
Vitalii Koval <vitalii.koval.dev@gmail.com>
2023-12-06 11:41:41 +0400

    FXD-66 Implement FathomPriceOracle

---
+++ contracts/main/apis/interfaces/IFathomOracleAggregator.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.8.17;
+
+interface IFathomOracleAggregator {
+    function getRoundData(uint80 _roundId)
+        external
+        view
+        returns (uint80 roundId, uint256 value, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+    function latestRoundData()
+        external
+        view
+        returns (uint80 roundId, uint256 value, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
