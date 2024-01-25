327df2174f5c9514b3dc2c692adde3e5a293432e
codinghistorian <gaudihaus@gmail.com>
2024-01-25 16:40:48 +0400

    OxorioFix2024Jan25I3 Unsafe usage of abi.encodeWithSelector in SafeToken
    in SafeToken lib,
    abi.encodeWithSelector -> abi.encodeCall

--- contracts/main/utils/SafeToken.sol
+++ contracts/main/utils/SafeToken.sol
@@ -5,6 +5,9 @@ import "@openzeppelin/contracts/utils/Address.sol";
 
 interface ERC20Interface {
     function balanceOf(address _user) external view returns (uint256);
+    function approve(address _to, uint256 _value) external returns (bool);
+    function transfer(address _to, uint256 _value) external returns (bool);
+    function transferFrom(address from, address _to, uint256 _value) external returns (bool);
 }
 
 library SafeToken {
@@ -18,19 +21,19 @@ library SafeToken {
 
     function safeApprove(address _token, address _to, uint256 _value) internal {
         require(Address.isContract(_token), "safeApprove: non-contract address");
-        (bool success, bytes memory data) = _token.call(abi.encodeWithSelector(0x095ea7b3, _to, _value)); // bytes4(keccak256(bytes('approve(address,uint256)')));
+        (bool success, bytes memory data) = _token.call(abi.encodeCall(ERC20Interface.approve, (_to, _value)));
         require(success && (data.length == 0 || abi.decode(data, (bool))), "!safeApprove");
     }
 
     function safeTransfer(address _token, address _to, uint256 _value) internal {
         require(Address.isContract(_token), "safeTransfer: non-contract address");
-        (bool success, bytes memory data) = _token.call(abi.encodeWithSelector(0xa9059cbb, _to, _value)); // bytes4(keccak256(bytes('transfer(address,uint256)')));
+        (bool success, bytes memory data) = _token.call(abi.encodeCall(ERC20Interface.transfer, (_to, _value)));
         require(success && (data.length == 0 || abi.decode(data, (bool))), "!safeTransfer");
     }
 
     function safeTransferFrom(address _token, address from, address _to, uint256 _value) internal {
         require(Address.isContract(_token), "safeTransferFrom: non-contract address");
-        (bool success, bytes memory data) = _token.call(abi.encodeWithSelector(0x23b872dd, from, _to, _value)); // bytes4(keccak256(bytes('transferFrom(address,address,uint256)')));
+        (bool success, bytes memory data) = _token.call(abi.encodeCall(ERC20Interface.transferFrom, (from, _to, _value)));
         require(success && (data.length == 0 || abi.decode(data, (bool))), "!safeTransferFrom");
     }
 
