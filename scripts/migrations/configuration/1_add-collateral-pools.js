383d17617c136978fb23bcb8b833eab593af3f6f
codinghistorian <gaudihaus@gmail.com>
2024-06-05 16:57:20 +0400

    FXD-127-20240605 pre-configurable init collateral

--- scripts/migrations/configuration/1_add-collateral-pools.js
+++ scripts/migrations/configuration/1_add-collateral-pools.js
@@ -1,7 +1,9 @@
 const pools = require("../../common/collateral");
 const { getAddresses } = require("../../common/addresses");
 const { getProxy } = require("../../common/proxies");
-const { getConfigInitialCollateral } = require("../../common/collateral-setup-helper");
+const { 
+    getConfig
+} = require("../../common/collateral-setup-helper");
 
 const { BigNumber } = require("ethers");
 const WeiPerWad = BigNumber.from(`1${"0".repeat(18)}`)
@@ -9,7 +11,7 @@ const WeiPerRay = BigNumber.from(`1${"0".repeat(27)}`)
 const WeiPerRad = BigNumber.from(`1${"0".repeat(45)}`)
 
 module.exports = async function (deployer) {
-    const config = getConfigInitialCollateral(deployer.networkId());
+    const config = getConfig(deployer.networkId());
     const CLOSE_FACTOR_BPS = BigNumber.from(config.CLOSE_FACTOR_BPS)   // <- 0.25
     const LIQUIDATOR_INCENTIVE_BPS = BigNumber.from(config.LIQUIDATOR_INCENTIVE_BPS)  // <- 1.05
     const TREASURY_FEE_BPS = BigNumber.from(config.TREASURY_FEE_BPS) // <- 0.8
