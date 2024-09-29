0eef7c47f37ccfc775a02f3d5ed695689210e846
Anton Grigorev <anton.grigorev.dev@gmail.com>
2024-09-30 01:33:43 +0400

    Scripts replaced

--- scripts/common/collateral-setup-helper.js
+++ scripts/common/collateral-setup-helper.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const { formatBytes32String } = require("ethers/lib/utils");
 const ZeroAddress = "0x0000000000000000000000000000000000000000"
 
-const rawdata = fs.readFileSync('../../initialCollateralSetUp.json');
+const rawdata = fs.readFileSync('../../op-config/initialCollateralSetUp.json');
 const config = JSON.parse(rawdata);
 
-const rawdata2 = fs.readFileSync('../../newCollateralSetup.json');
+const rawdata2 = fs.readFileSync('../../op-config/newCollateralSetup.json');
 const config2 = JSON.parse(rawdata2);
 
 function getConfigInitialCollateral(chainId)  {
